cd979e349db401c131f5477af216c18a6c5dff9a
Quang-Vinh <duongvinh1113@gmail.com>
2026-08-12 15:36:46 +0700

    Sua loi

--- ShopVanPhongPham/ShopVanPhongPham/obj/Debug/net8.0/ShopVanPhongPham.AssemblyInfo.cs
+++ ShopVanPhongPham/ShopVanPhongPham/obj/Debug/net8.0/ShopVanPhongPham.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopVanPhongPham")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b122a80e4cefc435d147852e0fa2c01a5bc46ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404104e0bc0d877ef502f2e859bc706e2bfcb15b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopVanPhongPham")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopVanPhongPham")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
