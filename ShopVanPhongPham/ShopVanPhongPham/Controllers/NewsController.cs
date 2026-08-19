49be7744dfb6f7c713b32b03cae074444cda97b6
NguyenNgocTan <Tan313431@gmail.com>
2026-08-19 20:01:01 +0700

    Tin tức

---
+++ ShopVanPhongPham/ShopVanPhongPham/Controllers/NewsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopVanPhongPham.Data;
+
+namespace ShopVanPhongPham.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public NewsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var newsList = _context.News.OrderByDescending(n => n.CreatedAt).ToList();
+            return View(newsList);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var news = _context.News.Find(id);
+            if (news == null) return NotFound();
+            return View(news);
+        }
+    }
+}
